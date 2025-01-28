4535d443e2883094c39914d49153f747c49397f5
djafarxyxy <farhanch12344@gmail.com>
2025-01-28 20:06:13 +0700

    model klien

---
+++ app/src/main/java/com/example/aplikasi1/data/model/Klien.kt
@@ -0,0 +1,26 @@
+package com.example.aplikasi1.data.model
+
+@Serializable
+data class Klien(
+    @SerialName("id_klien")
+    val idKlien: Int, // ID unik klien
+
+    @SerialName("nama_klien")
+    val namaKlien: String, // Nama klien
+
+    @SerialName("kontak_klien")
+    val kontakKlien: String // Informasi kontak klien (email, telepon)
+)
+
+data class KlienResponse(
+    val status: Boolean, // Status respon (berhasil/gagal)
+    val message: String, // Pesan respon
+    val data: List<Klien> // Daftar data klien
+)
+
+@Serializable
+data class KlienDetailResponse(
+    val status: Boolean, // Status respon (berhasil/gagal)
+    val message: String, // Pesan respon
+    val data: Klien // Detail data klien
+)
