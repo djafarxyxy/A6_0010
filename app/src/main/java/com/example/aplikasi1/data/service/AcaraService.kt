1a80e86122b4bf34e26d4ec731eaf15c47ed6d09
djafarxyxy <farhanch12344@gmail.com>
2025-01-31 14:13:06 +0700

    service acara

--- app/src/main/java/com/example/aplikasi1/data/service/AcaraService.kt
+++ app/src/main/java/com/example/aplikasi1/data/service/AcaraService.kt
@@ -1,25 +1,36 @@
 package com.example.aplikasi1.data.service
 
 import com.example.aplikasi1.data.model.Acara
+import com.example.aplikasi1.data.model.AcaraDetailResponse
+import com.example.aplikasi1.data.model.AcaraResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface AcaraService {
+
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json"
     )
 
-    @GET("acara")
-    suspend fun getAcara(): AcaraResponse
-
-    @GET("acara/{idAcara}")
-    suspend fun getAcaraById(@Path("idAcara") idAcara: Int): AcaraDetailResponse
-
-    @POST("acara/store")
+    @POST("insertacara.php")
     suspend fun insertAcara(@Body acara: Acara)
 
-    @PUT("acara/{idAcara}")
-    suspend fun updateAcara(@Path("idAcara") idAcara: Int, @Body acara: Acara)
+    @GET("bacaacara.php")
+    suspend fun getAllAcara(): List<Acara>
 
-    @DELETE("acara/{idAcara}")
-    suspend fun deleteAcara(@Path("idAcara") idAcara: Int): Response<Void>
+    @GET("baca1acara.php/{id_acara}")
+    suspend fun getAcaraById(@Query("id_acara") idAcara: String): Acara
+
+    @PUT("editacara.php/{id_acara}")
+    suspend fun updateAcara(@Query("id_acara") idAcara: String, @Body acara: Acara)
+
+    @DELETE("deleteacara.php/{id_acara}")
+    suspend fun deleteAcara(@Query("id_acara") idAcara: String): Response<Void>
 }
