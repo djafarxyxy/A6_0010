8ca1b69c2eedb94fa4fd4e3910dc0cc3ca11c1c7
djafarxyxy <farhanch12344@gmail.com>
2025-01-31 14:12:22 +0700

    repository acara

--- app/src/main/java/com/example/aplikasi1/data/repository/AcaraRepository.kt
+++ app/src/main/java/com/example/aplikasi1/data/repository/AcaraRepository.kt
@@ -1,34 +1,43 @@
 package com.example.aplikasi1.data.repository
 
 import com.example.aplikasi1.data.model.Acara
+import com.example.aplikasi1.data.model.AcaraResponse
 import com.example.aplikasi1.data.service.AcaraService
 import java.io.IOException
 
 
+interface AcaraRepository {
+
+    suspend fun insertAcara(acara: Acara)
+
+    suspend fun getAcara(): List<Acara>
+
+    suspend fun updateAcara(id: String, acara: Acara)
+
+    suspend fun deleteAcara(id: String)
+
+    suspend fun getAcarabyId(id: String): Acara
+}
+
 class NetworkAcaraRepository(
-    private val acaraApiService : AcaraService
+    private val acaraApiService: AcaraService
 ) : AcaraRepository {
-    override suspend fun getAcara(): AcaraResponse {
-        return try {
-            acaraApiService.getAcara()
-        } catch (e: Exception) {
-            throw IOException("Failed to fetch acara data : ${e.message}")
-        }
-    }
 
     override suspend fun insertAcara(acara: Acara) {
         acaraApiService.insertAcara(acara)
     }
 
-    override suspend fun updateAcara(idacara: Int, acara: Acara) {
-        acaraApiService.updateAcara(idAcara, acara)
+    override suspend fun updateAcara(id: String, acara: Acara) {
+        acaraApiService.updateAcara(id, acara)
     }
 
-    override suspend fun deleteAcara(idAcara: Int) {
+    override suspend fun deleteAcara(id: String) {
         try {
-            val response = acaraApiService.deleteAcara(idAcara)
+            val response = acaraApiService.deleteAcara(id)
             if (!response.isSuccessful) {
-                throw IOException("gagal menghapus data Acara. HTTP kode: ${response.code()}")
+                throw IOException(
+                    "Failed to delete acara. HTTP Status Code: ${response.code()}"
+                )
             } else {
                 response.message()
                 println(response.message())
@@ -38,8 +47,10 @@ class NetworkAcaraRepository(
         }
     }
 
-    override suspend fun getAcaraById(idAcara: Int): Acara {
-        return acaraApiService.getAcaraById(idAcara).data
-    }
+    override suspend fun getAcara(): List<Acara> =
+        acaraApiService.getAllAcara()
 
+    override suspend fun getAcarabyId(id: String): Acara {
+        return acaraApiService.getAcaraById(id)
+    }
 }
