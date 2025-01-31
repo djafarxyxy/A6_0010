cdba15dc9521e7cc7e21856a34ba5c78f46fe6f6
djafarxyxy <farhanch12344@gmail.com>
2025-01-31 14:17:51 +0700

    i vendor view model

---
+++ app/src/main/java/com/example/aplikasi1/ui/viewmodel/vendor/InsertVendorViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.aplikasi1.ui.viewmodel.vendor
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.aplikasi1.data.model.Vendor
+import com.example.aplikasi1.data.repository.VendorRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModelVendor(private val vendorRepository: VendorRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertVendorState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+
+    fun insertVendor() {
+        viewModelScope.launch {
+            try {
+                val vendor = uiState.insertUiEvent.toVendor()
+                vendorRepository.insertVendor(vendor)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = uiState.copy(error = e.message)
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+    val error: String? = null
+)
+
+data class InsertUiEvent(
+    val idVendor: String = "",
+    val namaVendor: String = "",
+    val jenisVendor: String = "",
+    val kontakVendor: String = ""
+)
+
+fun InsertUiEvent.toVendor(): Vendor = Vendor (
+    idVendor = idVendor,
+    namaVendor = namaVendor,
+    jenisVendor = jenisVendor,
+    kontakVendor = kontakVendor
+)
+
+fun Vendor.toUiStateVendor(): InsertUiState = InsertUiState(
+    insertUiEvent = this.toInsertUiEvent()
+)
+
+fun Vendor.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    idVendor = idVendor,
+    namaVendor = namaVendor,
+    jenisVendor = jenisVendor,
+    kontakVendor = kontakVendor
+
+)
